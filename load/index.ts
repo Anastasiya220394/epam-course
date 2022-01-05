940fe074f75a84d70f02f0ec1f763829074fdd16
anastasiya220394 <anonim22@icloud.com>
2022-01-05 14:33:01 +0300

    refactor: changed indentations in index.ts

--- load/index.ts
+++ load/index.ts
@@ -3,55 +3,53 @@ interface ICustomResponse extends XMLHttpRequest {
 }
 
 interface IObject {
-	method?: string;
-	body?: BodyInit | object | null;
+  method?: string;
+  body?: BodyInit | object | null;
   responseType?: XMLHttpRequestResponseType;
   signal?: AbortSignal;
 }
 
 function load(url:string, obj:IObject = {method:'', body: '', responseType:''}) {
   return new Promise(function (resolve, reject) {
+    const method = obj.method || "GET";
+    const body = obj.body || '';
+    const responseType = obj.responseType || 'json';
 
-      const method = obj.method || "GET";
-      const body = obj.body || '';
-      const responseType = obj.responseType || 'json';
+    const xhr:ICustomResponse = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = responseType;
+    xhr.setRequestHeader('Content-Type', 'application/json');
 
-      const xhr:ICustomResponse = new XMLHttpRequest();
-      xhr.open(method, url);
-      xhr.responseType = responseType;
-      xhr.setRequestHeader('Content-Type', 'application/json');
-
-      xhr.onload = function () {
-          if ( xhr.status >= 200 && xhr.status < 300 ) {
-              resolve({
-                  status: xhr.status,
-                  body: xhr.response as Response,
-                  statusText: xhr.statusText     
-              });
-          }
-          else {
-              reject({
-                  status: xhr.status,
-                  body: xhr.response as Response,
-                  statusText: xhr.statusText     
-              });
-          }
-      };
-
-      xhr.onerror = function () {
-          reject(xhr.response);
-      };
-      
-      if(obj.signal) {
-          obj.signal.addEventListener('abort', () => {
-            xhr.abort();
-          });
-      }
-
-      if(method === 'DELETE') {
-          xhr.send();
+    xhr.onload = function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve({
+          status: xhr.status,
+          body: xhr.response as Response,
+          statusText: xhr.statusText     
+        });
       } else {
-          xhr.send(JSON.stringify(body));
+        reject({
+          status: xhr.status,
+          body: xhr.response as Response,
+          statusText: xhr.statusText     
+        });
       }
+    };
+
+    xhr.onerror = function () {
+      reject(xhr.response);
+    };
+      
+    if(obj.signal) {
+      obj.signal.addEventListener('abort', () => {
+        xhr.abort();
+      });
+    }
+
+    if(method === 'DELETE') {
+      xhr.send();
+    } else {
+      xhr.send(JSON.stringify(body));
+    }    
   });
 }
