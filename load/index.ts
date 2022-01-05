bb261e960ca328ca5f2dc8512d6c85322995f081
anastasiya220394 <anonim22@icloud.com>
2022-01-05 14:27:15 +0300

    fixed: function load, refactor: deleted added files

--- load/index.ts
+++ load/index.ts
@@ -1,42 +1,57 @@
-const requestURL:string = 'https://jsonplaceholder.typicode.com/users';
-function sendRequest(method:string, url:string, body: object | null = null) {
-  return new Promise<string>((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
+interface ICustomResponse extends XMLHttpRequest {
+  readonly response: Response;
+}
 
-    xhr.open(method, url);
+interface IObject {
+	method?: string;
+	body?: BodyInit | object | null;
+  responseType?: XMLHttpRequestResponseType;
+  signal?: AbortSignal;
+}
 
-    xhr.responseType = 'json';
-    xhr.setRequestHeader('Content-Type', 'application/json');
+function load(url:string, obj:IObject = {method:'', body: '', responseType:''}) {
+  return new Promise(function (resolve, reject) {
 
-    xhr.onload = () => {
-      if (xhr.status >= 400) {
-        reject(xhr.response)
-      } else {
-        resolve(xhr.response)
+      const method = obj.method || "GET";
+      const body = obj.body || '';
+      const responseType = obj.responseType || 'json';
+
+      const xhr:ICustomResponse = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.responseType = responseType;
+      xhr.setRequestHeader('Content-Type', 'application/json');
+
+      xhr.onload = function () {
+          if ( xhr.status >= 200 && xhr.status < 300 ) {
+              resolve({
+                  status: xhr.status,
+                  body: xhr.response as Response,
+                  statusText: xhr.statusText     
+              });
+          }
+          else {
+              reject({
+                  status: xhr.status,
+                  body: xhr.response as Response,
+                  statusText: xhr.statusText     
+              });
+          }
+      };
+
+      xhr.onerror = function () {
+          reject(xhr.response);
+      };
+      
+      if(obj.signal) {
+          obj.signal.addEventListener('abort', () => {
+            xhr.abort();
+          });
       }
-    }
 
-    xhr.onerror = () => {
-      reject(xhr.response)
-    }
-
-    if(method === 'DELETE') {
-      xhr.send()
-    } else {
-      xhr.send(JSON.stringify(body))
-    }
-  })
+      if(method === 'DELETE') {
+          xhr.send();
+      } else {
+          xhr.send(JSON.stringify(body));
+      }
+  });
 }
-
-/*sendRequest('GET', requestURL)
-  .then(data => console.log(data))
-  .catch(err => console.log(err))
-
-const body = {
-  name: 'Vladilen',
-  age: 26
-}
-
-sendRequest('DELETE', 'https://jsonplaceholder.typicode.com/posts/1')
-  .then(data => console.log(data))
-  .catch(err => console.log(err))*/
