644fc7b8e3e389f17d00de1ae69b027d6e36ff3b
anastasiya220394 <anonim22@icloud.com>
2022-02-11 18:43:12 +0300

    refactor: add eslint and package.json

--- load/index.ts
+++ load/index.ts
@@ -1,56 +1,55 @@
 interface ICustomResponse extends XMLHttpRequest {
-    readonly response: Response;
-  }
-  
-  interface IObject {
-    method?: string;
-    body?: BodyInit | object | null;
-    responseType?: XMLHttpRequestResponseType;
-    signal?: AbortSignal;
-  }
-  
-  function load(url:string, obj:IObject = {method:'', body: '', responseType:''}) {
-    return new Promise(function (resolve, reject) {
-      const method = obj.method || "GET";
-      const body = obj.body || '';
-      const responseType = obj.responseType || 'json';
-  
-      const xhr:ICustomResponse = new XMLHttpRequest();
-      xhr.open(method, url);
-      xhr.responseType = responseType;
-      xhr.setRequestHeader('Content-Type', 'application/json');
-  
-      xhr.onload = function () {
-        if (xhr.status >= 200 && xhr.status < 300) {
-          resolve({
-            status: xhr.status,
-            body: xhr.response as Response,
-            statusText: xhr.statusText     
-          });
-        } else {
-          reject({
-            status: xhr.status,
-            body: xhr.response as Response,
-            statusText: xhr.statusText     
-          });
-        }
-      };
-  
-      xhr.onerror = function () {
-        reject(xhr.response);
-      };
-        
-      if(obj.signal) {
-        obj.signal.addEventListener('abort', () => {
-          xhr.abort();
+  readonly response: Response;
+}
+
+interface IObject {
+  method?: string;
+  body?: BodyInit | object | null;
+  responseType?: XMLHttpRequestResponseType;
+  signal?: AbortSignal;
+}
+
+function load(url:string, obj:IObject = { method: '', body: '', responseType: '' }) {
+  return new Promise((resolve, reject) => {
+    const method = obj.method || 'GET';
+    const body = obj.body || '';
+    const responseType = obj.responseType || 'json';
+
+    const xhr:ICustomResponse = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = responseType;
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onload = function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve({
+          status: xhr.status,
+          body: xhr.response as Response,
+          statusText: xhr.statusText,
+        });
+      } else {
+        reject({
+          status: xhr.status,
+          body: xhr.response as Response,
+          statusText: xhr.statusText,
         });
       }
-  
-      if(method === 'DELETE') {
-        xhr.send();
-      } else {
-        xhr.send(JSON.stringify(body));
-      }    
-    });
-  }
-  
+    };
+
+    xhr.onerror = function () {
+      reject(xhr.response);
+    };
+
+    if (obj.signal) {
+      obj.signal.addEventListener('abort', () => {
+        xhr.abort();
+      });
+    }
+
+    if (method === 'DELETE') {
+      xhr.send();
+    } else {
+      xhr.send(JSON.stringify(body));
+    }
+  });
+}
